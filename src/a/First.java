cf7209301204f80008113ee1a95ed84fadf951c4
tjfgk97 <tjfgk97@gmail.com>
2023-11-29 15:39:27 +0900

    first commit

---
+++ src/a/First.java
@@ -0,0 +1,12 @@
+//현재 클래스(자바파일)가 소속된 패키지의 이름
+package a;
+
+//현재 클래스의 이름
+public class First {
+	//main 메서드(프로그램의 시작 지점)
+	public static void main(String[] args) {
+		// 실행할 내용이 작성되는 공간
+		System.out.println("자바야 앞으로 친하게 지내자..");
+	}
+
+} // First class 블록의 끝
